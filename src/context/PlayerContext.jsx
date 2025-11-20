13c7d7e7e83d9ed94eff821f26847a91df545ed1
Ghadi <ghadi.dababneh@gmail.com>
2025-11-20 17:21:20 +0100

    Create Seekbar functionality

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -32,18 +32,46 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false)
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+
+    const previous = async () => {
+        if (track.id > 0) {
+            await setTrack(songsData[track.id - 1]);
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const next = async () => {
+        if (track.id < songsData.length - 1) {
+            await setTrack(songsData[track.id + 1]);
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const seekSong = async (e) => {
+        audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.clientWidth) * audioRef.current.duration);
+    }
+
     useEffect(() => {
         setTimeout(() => {
 
             audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100)) + "%"
                 setTime({
                     currentTime: {
-                        second: Math.floor(audioRef.current.currentTime % 60),
-                        minute: Math.floor(audioRef.current.currentTime / 60)
+                        second: Math.floor(audioRef.current.currentTime % 60).toString().padStart(2, '0'),
+                        minute: Math.floor(audioRef.current.currentTime / 60).toString().padStart(2, '0')
                     },
                     totalTime: {
-                        second: Math.floor(audioRef.current.duration % 60),
-                        minute: Math.floor(audioRef.current.duration / 60)
+                        second: Math.floor(audioRef.current.duration % 60).toString().padStart(2, '0'),
+                        minute: Math.floor(audioRef.current.duration / 60).toString().padStart(2, '0')
                     }
                 })
             }
@@ -55,14 +83,13 @@ const PlayerContextProvider = (props) => {
         audioRef,
         seekBar,
         seekBg,
-        track,
-        setTrack,
-        playStatus,
-        setPlayStatus,
-        time,
-        setTime,
-        play,
-        pause
+        track,setTrack,
+        playStatus,setPlayStatus,
+        time,setTime,
+        play,pause,
+        playWithId,
+        previous,next,
+        seekSong
     }
 
     return (
